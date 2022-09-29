847d775498f8211e3c56a653c02f19a2a6654bc7
Misbahul Amin <misbahul.amin2001@gmail.com>
2022-09-29 20:46:36 +0600

    shoppint cart app updated

--- js/case.js
+++ js/case.js
@@ -1,3 +1,30 @@
+function updateCaseNumber(isIncrease) {
+    const caseIputField = document.getElementById('case-number-field');
+    const caeInputFieldString = caseIputField.value;
+    const caeInputFieldNumber = parseInt(caeInputFieldString);
+    let newCaseNumberField;
+    if (isIncrease === true) {
+        newCaseNumberField = caeInputFieldNumber + 1;
+    }
+    else {
+        newCaseNumberField = caeInputFieldNumber - 1;
+    }
+    if (newCaseNumberField >= 0) {
+        caseIputField.value = newCaseNumberField;
+    }
+    
+    return newCaseNumberField;
+}
+
+
+
 document.getElementById('btn-case-price').addEventListener('click', function () {
-    console.log("Button clicked here");
+    const newCaseNumberField = updateCaseNumber(true);
+    // caseIputField.value = newCaseNumberField;
+})
+
+document.getElementById('btn-case-minus').addEventListener('click', function () {
+    const newCaseNumberField = updateCaseNumber(false);
+    // caseIputField.value = newCaseNumberField;
+    
 })
